bc8242b4727bfbda1b19a2d6364bb4dde5960ef8
Digão Reis <digaoreis.dev@gmail.com>
2022-11-06 20:35:21 -0300

    Fim Estrutura da Aplicação

--- 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
@@ -1,4 +1,5 @@
 import { HandPalm, Knife } from 'phosphor-react'
+import { Avatar } from './Avatar';
 import { Comment } from './Comment';
 import styles from './Post.module.css';
 
@@ -7,8 +8,7 @@ export function Post(){
     <article className={ styles.post }>
       <header>
         <div className={ styles.author }>
-          <img
-            className={ styles.avatar }
+          <Avatar
             src="https://github.com/digaoreisdev.png"
           />
 
