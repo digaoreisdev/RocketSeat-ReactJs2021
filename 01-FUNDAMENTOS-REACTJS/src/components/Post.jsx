1fe0b8529d5e782f86ff476fb611a78c5f000962
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-11-08 18:41:46 -0300

    Post 02

--- 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
@@ -1,35 +1,42 @@
-import { HandPalm, Knife } from 'phosphor-react'
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+
 import styles from './Post.module.css';
 
-export function Post(){
+// author: { avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+export function Post({ author, publishedAt }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+  })
+
   return (
     <article className={ styles.post }>
       <header>
         <div className={ styles.author }>
-          <Avatar
-            src="https://github.com/digaoreisdev.png"
-          />
-
+          <Avatar src={author.avatarUrl} />
           <div className={ styles.authorInfo}>
-            <strong> Rodrigo Reis </strong>
-            <span> Web Developer </span>
+            <strong> {author.name} </strong>
+            <span> {author.role} </span>
           </div>
         </div>
 
-        <time title="11 de maio às 12:28hs" dateTime="2022-05-11 08:13:30"> Publicado há 1hs </time>
+        <time title={publishedDateFormatted} dateTime="2022-05-11 08:13:30">
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={ styles.content }>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p>👉{' '} <a href=""> jane.design/doctorcare</a></p>
-        <p>
-          <a href=""> #novoprojeto </a>{' '}
-          <a href=""> #nlw </a>{' '}
-          <a href=""> #rocketseat </a>
-        </p>
+        
       </div>
 
       <form className={ styles.commentForm }>
