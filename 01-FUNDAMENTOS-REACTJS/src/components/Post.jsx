a6c27edfce8e72764418b8ad077aa160bcda6b76
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-11-10 17:36:28 -0300

    TypeScript

--- 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
+++ 01-FUNDAMENTOS-REACTJS/src/components/Post.jsx
@@ -41,9 +41,25 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
   }
 
+  function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Este campo é Obrigatório!')
+  }
+
+  function deleteComment(commentToDelete) {
+    // Imutabilidade --> as variáveis não sofrem mutação, nós criamos um novo valor (Um novo espaço na memória)
+    const commentsWithoutDeleteOne = comments.filter(comment => {
+      return comment !== commentToDelete; 
+    })
+
+    setComments(commentsWithoutDeleteOne);
+  }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
+  
   return (
     <article className={ styles.post }>
       <header>
@@ -63,9 +79,9 @@ export function Post({ author, publishedAt, content }) {
       <div className={ styles.content }>
         {content.map(line => {
           if (line.type === 'paragraph') {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === 'link') {
-            return <p> <a href="">{line.content}</a> </p>
+            return <p key={line.content}> <a href="">{line.content}</a> </p>
           }
         })}
       </div>
@@ -78,16 +94,29 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button
+            type="submit"
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
       <div className={styles.commentList}></div>
         {comments.map(comment => {
-          return <Comment content={comment} />
+          return (
+            <Comment
+              key={comment}
+              content={comment}
+              onDeleteComment={deleteComment}
+            />
+          )
         })}
     </article>    
   )
