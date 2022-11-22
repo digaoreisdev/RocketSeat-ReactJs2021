cfad6382083e12c5513ac486e6e8b77a450056e9
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-11-22 13:53:18 -0300

    Página Home Finalizada

--- 02-IGNITE-TIME/src/pages/Home/index.tsx
+++ 02-IGNITE-TIME/src/pages/Home/index.tsx
@@ -1,19 +1,38 @@
 import { Play } from "phosphor-react"
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles"
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles"
 
-export function Home(){
+export function Home() {
   return (
     <HomeContainer>
       <form action="">
         <FormContainer>
-        <label htmlFor="task">Vou trabalhar em</label>
-        <input id="task" />
 
-        <label htmlFor="minutesAmount">durante</label>
-        <input type="number" id="minutesAmount" />
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="task-suggestions"
+            placeholder="Dê um nome para o seu projeto"
+          />
 
-        <span>minutos.</span>
-        </FormContainer>      
+          <datalist id="task-suggestions">
+            <option value="Projeto1" />
+            <option value="Projeto2" />
+            <option value="Projeto3" />
+            <option value="Banana" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
 
         <CountdownContainer>
           <span>0</span>
@@ -23,10 +42,10 @@ export function Home(){
           <span>0</span>
         </CountdownContainer>
 
-        <button type="submit">
-          <Play size={24}/>
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
           Começar
-        </button>
+        </StartCountdownButton>
       </form>
     </HomeContainer>
   )
