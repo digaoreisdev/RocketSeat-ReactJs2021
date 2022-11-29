643e23060dce5896a3ef4cf4d74916dae4c1e6fb
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-11-29 12:04:03 -0300

    Contexto no React - Fim

--- 02-IGNITE-TIME/src/pages/History/index.tsx
+++ 02-IGNITE-TIME/src/pages/History/index.tsx
@@ -1,7 +1,8 @@
-import { CircleWavyCheck } from 'phosphor-react';
 import { useContext } from 'react'
+import { formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 import { CyclesContext } from '../../contexts/CyclesContext'
-import { HistoryContainer, HistoryList, Status } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles"
 
 export function History() {
   const { cycles } = useContext(CyclesContext)
@@ -25,8 +26,13 @@ export function History() {
               return (
                 <tr key={cycle.id}>
                   <td>{cycle.task}</td>
-                  <td>{cycle.minutesAmount}</td>
-                  <td>{cycle.startDate.toISOString()}</td>
+                  <td>{cycle.minutesAmount} minutos</td>
+                  <td>
+                    {formatDistanceToNow(cycle.startDate, {
+                      addSuffix: true,
+                      locale: ptBR,
+                    })}
+                  </td>  
                   <td>
                     { cycle.finishedDate && (
                       <Status statusColor="green">Concluido</Status>
