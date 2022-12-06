739d5c0cf0d5b220613b999e2bc3cc9ebe0c3f73
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-12-06 10:22:01 -0300

    Tabela de Transações

--- 03-dt-Money/src/pages/Transactions/index.tsx
+++ 03-dt-Money/src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 
 export function Transactions() {
@@ -7,6 +8,38 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+      
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              
+              <td>
+                <PriceHighLight variant="income">
+                  R$ 12.000,00
+                </PriceHighLight>
+              </td>
+              
+              <td>Venda</td>
+              
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              
+              <td>
+                <PriceHighLight variant="outcome">
+                - R$ 59,00
+                </PriceHighLight>
+              </td>
+              
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
