c84d012b7e459272334db0a7555c480a2df49ec7
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2022-12-09 16:47:13 -0300

    Criando Modal Finalizado

--- 03-dt-Money/src/components/header/index.tsx
+++ 03-dt-Money/src/components/header/index.tsx
@@ -1,24 +1,33 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
-import * as Dialog from '@radix-ui/react-dialog';
-import logoImg from '../../assets/logo.svg';
+
+import logoImg from "../../assets/logo.svg";
+import * as Dialog from "@radix-ui/react-dialog"
+// import { Dialog } from "@radix-ui/react-dialog";
+// import { Root as DialogRoot } from '@radix-ui/react-dialog';
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
-        <img src={ logoImg } alt="" />
+        <img src={logoImg} alt="" />
         
         <Dialog.Root>
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
           
-          <NewTransactionModal>
+          <Dialog.Portal>
+            <Dialog.Overlay />
             
-          </NewTransactionModal>
-
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
         </Dialog.Root>
-      </HeaderContent>
+        
+        </HeaderContent>
     </HeaderContainer>
   )
 }
