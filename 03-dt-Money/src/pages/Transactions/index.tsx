4f15ba7df3e73a523410b4583e86c57b4d492c7f
Digão Reis <93385240+digaoreisdev@users.noreply.github.com>
2023-01-02 17:55:12 -0300

    Realizando Requisição HTTP

--- 03-dt-Money/src/pages/Transactions/index.tsx
+++ 03-dt-Money/src/pages/Transactions/index.tsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
+interface Transactions {
+  id: number;
+  description: string;
+  type: 'income' | 'outcome';
+  price: number;
+  category: string;
+  createdAt: string;
+}
 
 export function Transactions() {
+  const [transactions, setTransactions] = useState<Transactions[]>([])
+  
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:5000/transactions')
+    const data = await response.json();
+    
+    setTransactions(data);
+  }
+  
+  useEffect(() => {
+    loadTransactions();
+  }, [])
+  
   return (
     <div>
       <Header />
@@ -15,31 +37,24 @@ export function Transactions() {
         
         <TransactionsTable>
           <tbody>
-            <tr>
-              <td width="50%">Desenvolvimento de Site</td>
-              
-              <td>
-                <PriceHighLight variant="income">
-                  R$ 12.000,00
-                </PriceHighLight>
-              </td>
-              
-              <td>Venda</td>
-              
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td width="50%">Hamburguer</td>
-              
-              <td>
-                <PriceHighLight variant="outcome">
-                - R$ 59,00
-                </PriceHighLight>
-              </td>
-              
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map(transaction => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.price}
+                    </PriceHighLight>
+                  </td>
+                  
+                  <td>{transaction.category}</td>
+                  
+                  <td>{transaction.createdAt}</td>
+                </tr>
+              )
+            })}
+            
           </tbody>
         </TransactionsTable>
       </TransactionsContainer>
